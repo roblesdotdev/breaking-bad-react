7a3eea863d313260be6581adf00abe1c1c5869bf
roblesdotdev <httpsnip@gmail.com>
2022-09-22 23:03:48 -0300

    card styles

--- src/components/CharacterDataView.jsx
+++ src/components/CharacterDataView.jsx
@@ -1,31 +1,20 @@
 function CharacterDataView({character}) {
   return (
-    <div>
-      <div style={{width: '100%', height: '350px', backgroundColor: 'white'}}>
+    <div className="card">
+      <div>
         {character.img ? (
-          <img
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              objectPosition: 'center',
-            }}
-            src={character.img}
-            alt={character.name}
-          />
+          <img className="card-img" src={character.img} alt={character.name} />
         ) : null}
       </div>
-      <section>
+      <div className="card-description">
         <h2>{character.name}</h2>
         <h3>{character.nickname}</h3>
-      </section>
-      <footer>
         <ul>
           {character.occupation?.map((el, idx) => (
             <li key={idx}>{el}</li>
           ))}
         </ul>
-      </footer>
+      </div>
     </div>
   )
 }
